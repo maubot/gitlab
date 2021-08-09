cfdb78b29b123305b973a8d1ad4c348563dc431b
Lars Kruse <devel@sumpfralle.de>
2021-08-09 23:34:32 +0200

    Fix exception in whoami command
    The `URL` import went missing in aa22103594 and thus an exception
    is raised, when the `whoami` command is executed.

--- gitlab_matrix/commands/server.py
+++ gitlab_matrix/commands/server.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from gitlab import Gitlab as Gl, GitlabAuthenticationError
+from yarl import URL
 
 from maubot.handlers import command
 from maubot import MessageEvent
